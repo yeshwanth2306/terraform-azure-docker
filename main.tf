34ec325fb2fea5806055f6c840fa12cfc2bff039
Yeshwanth B <ayashwanthbabu66@gmail.com>
2026-09-23 10:46:53 +0530

    Add virtual network and subnet resources

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "docker_rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_virtual_network" "docker_vnet" {
+  name                = "docker-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.docker_rg.location
+  resource_group_name = azurerm_resource_group.docker_rg.name
+}
+
+resource "azurerm_subnet" "docker_subnet" {
+  name                 = "docker-subnet"
+  resource_group_name  = azurerm_resource_group.docker_rg.name
+  virtual_network_name = azurerm_virtual_network.docker_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
