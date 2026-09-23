be7470ec20597b6724530c302bad903115ff2128
Yeshwanth B <ayashwanthbabu66@gmail.com>
2026-09-23 13:54:54 +0530

    Add security rule to allow HTTP traffic

--- main.tf
+++ main.tf
@@ -54,8 +54,19 @@ resource "azurerm_network_security_group" "docker_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-}
 
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
 resource "azurerm_network_interface_security_group_association" "docker_nsg_association" {
   network_interface_id      = azurerm_network_interface.docker_nic.id
   network_security_group_id = azurerm_network_security_group.docker_nsg.id
