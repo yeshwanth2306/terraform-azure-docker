65846e791b134e17bb0616b4382693388839e550
Yeshwanth B <ayashwanthbabu66@gmail.com>
2026-09-23 11:04:00 +0530

    Add Azure Linux VM resource configuration

--- main.tf
+++ main.tf
@@ -60,3 +60,34 @@ resource "azurerm_network_interface_security_group_association" "docker_nsg_asso
   network_interface_id      = azurerm_network_interface.docker_nic.id
   network_security_group_id = azurerm_network_security_group.docker_nsg.id
 }
+
+resource "azurerm_linux_virtual_machine" "docker_vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.docker_rg.name
+  location            = azurerm_resource_group.docker_rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.docker_nic.id
+  ]
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
