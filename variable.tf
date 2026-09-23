d21b980dffb36ee8537a55b0c93a6b3016fd6f43
Yeshwanth B <ayashwanthbabu66@gmail.com>
2026-09-23 10:43:37 +0530

    Define variables for Azure resources in variable.tf
    Added variables for resource group, location, VM name, and admin username in Terraform configuration.

--- variable.tf
+++ variable.tf
@@ -1,1 +1,23 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "rg-docker-practice"
+}
 
+variable "location" {
+  description = "Azure region for the resources"
+  type        = string
+  default     = "Central India"
+}
+
+variable "vm_name" {
+  description = "Name of the Linux virtual machine"
+  type        = string
+  default     = "docker-vm"
+}
+
+variable "admin_username" {
+  description = "Administrator username for the VM"
+  type        = string
+  default     = "azureuser"
+}
